0fe0378c6e04a8985739c675194c5fd64efac952
mihoyo-typ <32317812+mihoyo-typ@users.noreply.github.com>
2025-07-06 20:42:01 +0800

    Update AutoJoinTF.js

--- Tasks/AutoJoinTF.js
+++ Tasks/AutoJoinTF.js
@@ -51,7 +51,7 @@ const [
     UserAgent,
     // ----------
     // 应用参数
-    APP_ID_Str,
+       APP_ID_Str = "dDtSst46", // 写死为dDtSst46 
     // ----------
     // 配置参数
     LOON_COUNT = 1, // 每次执行循环执行多少次 默认1
